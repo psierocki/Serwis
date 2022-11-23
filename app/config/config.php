0d6f68a944f5e0bdbee78ac4f8174c9788b72bdc
Psierocki <patryk.sierocki16@gmail.com>
2022-11-23 17:31:39 +0100

    Delete

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$hostname = 'localhost';
-$db = 'serwis';
-$username = 'root';
-$password = '';
-
-@ $db = new mysqli($hostname,$username,$password,$db);
-$db-> query("SET NAMES 'utf8' COLLATE 'utf8_general_ci'");
